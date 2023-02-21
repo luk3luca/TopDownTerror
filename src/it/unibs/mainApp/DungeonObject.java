486efdf57c3bb586214773c983d3ddda4cbdfa21
Luca Martinelli <lucamartinelli02@gmail.com>
2023-02-21 13:12:38 +0100

    .

--- src/it/unibs/mainApp/DungeonObject.java
+++ src/it/unibs/mainApp/DungeonObject.java
@@ -4,9 +4,15 @@ import java.awt.Color;
 import java.awt.Shape;
 
 public class DungeonObject {
-	private double[] position = {0, 0};
+	private double[] position = {0, 0, 0};
 	private Shape shape;
 	private Color color;
+	
+	public DungeonObject(double[] position, Shape shape, Color color) {
+		this.position = position;
+		this.shape = shape;
+		this.color = color;
+	}
 	
 	
 }

a4582a11fb927b1247687fa87a56b23abc9f2ce5
Luca Martinelli <lucamartinelli02@gmail.com>
2023-02-21 13:26:49 +0100

    .

--- src/it/unibs/mainApp/Tile.java
+++ src/it/unibs/mainApp/Tile.java
@@ -4,7 +4,9 @@ import java.awt.Color;
 import java.awt.Shape;
 
 public abstract class Tile extends DungeonObject{
-
+	private boolean walkable;
+	private int dimension;
+	
 	public Tile(double[] position, Shape shape, Color color) {
 		super(position, shape, color);
 		

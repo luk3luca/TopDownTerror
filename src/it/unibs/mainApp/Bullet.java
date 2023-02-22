5bdb61d733a5682e1c410552336da978d7f4ee28
PatrickPasolini <patrickpasolini01@gmail.com>
2023-02-22 16:33:27 +0100

    .

--- src/it/unibs/mainApp/Bullet.java
+++ src/it/unibs/mainApp/Bullet.java
@@ -1,13 +1,38 @@
 package it.unibs.mainApp;
 
 import java.awt.Color;
-import java.awt.Shape;
+import java.awt.Polygon;
+import java.awt.geom.Area;
 
 public class Bullet extends DungeonObject {
 
-	public Bullet(double[] position, Shape shape, Color color) {
-		super(position, shape, color);
-		// TODO Auto-generated constructor stub
-	}
+	protected Player player;
+	protected Gun gun;
 
+	private int fuel;
+		
+	public Bullet(Player p, Gun g) {
+
+		this.player = p;
+		this.gun = g;
+		//TODO velocità fissa in base alla velocità del player
+		
+		this.shape = new Area (new Polygon(
+				new int[] {-5,0,5,0,-5},
+				new int[] {3,3,0,-3,-3},
+				5						 
+				));
+		
+	}
+	
+	
+	@Override
+	public void stepNext() {
+		super.stepNext();
+		
+		if(--fuel <=0) {
+			isAlive = false;
+			this.setVelocity(0, 0, 0);
+		}
+		
 }
---
+++ src/it/unibs/mainApp/MovementObject.java
@@ -0,0 +1,39 @@
+package it.unibs.mainApp;
+
+import java.awt.geom.Area;
+
+import it.unibs.pajc.SpaceObject;
+
+public class MovementObject {
+	
+	private static final double VELOCITY = 100.;
+	
+	protected double[] position = {0,0,0};
+	protected double velocity = VELOCITY;
+	protected boolean isAlive = true;
+
+	public boolean isAlive() {
+		return isAlive;
+	}
+	
+	public void rotate (double r) {
+		position[2] += r;
+	}
+	
+	
+	public void stepNext() {
+		position[0] = position[0] + VELOCITY;
+		position[1] = position[1] + VELOCITY;
+		position[2] = position[2] + VELOCITY;
+	}
+	
+	//TODO metodi per le collisioni 
+	
+	
+	/*GETTERS AND SETTERS*/
+	public void setPosition(double x,double y, double r) {
+		this.position[0] = x;
+		this.position[1] = y;
+		this.position[2] = r;
+	}
+}

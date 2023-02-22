4ac21a8719d41a430a35fff880709d2bed155a02
Luca Martinelli <lucamartinelli02@gmail.com>
2023-02-22 17:27:23 +0100

    Map

--- src/it/unibs/mainApp/MainApp.java
+++ src/it/unibs/mainApp/MainApp.java
@@ -36,7 +36,7 @@ public class MainApp {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 600, 800);
+		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
--- src/it/unibs/mainApp/Map.java
+++ src/it/unibs/mainApp/Map.java
@@ -1,12 +1,26 @@
 package it.unibs.mainApp;
 
+import java.awt.*;
+
 import javax.swing.JPanel;
 
 public class Map extends JPanel {
+	
 	/**
 	 * Create the panel.
 	 */	
 	public Map() {
+		
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		
+		
+		
 		
 	}
 

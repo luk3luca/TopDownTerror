775ec6581fbbe15450de72b7b1f039f9236dc1b5
Luca Martinelli <lucamartinelli02@gmail.com>
2023-02-22 17:36:21 +0100

    .

---
+++ src/it/unibs/mainApp/PnlMap.java
@@ -0,0 +1,27 @@
+package it.unibs.mainApp;
+
+import java.awt.*;
+
+import javax.swing.JPanel;
+
+public class PnlMap extends JPanel {
+	/**
+	 * Create the panel.
+	 */	
+	public PnlMap() {
+		
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2 = (Graphics2D)g;
+		
+		
+		
+		
+		
+	}
+	
+	
+}
